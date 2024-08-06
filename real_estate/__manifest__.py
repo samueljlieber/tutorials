a8a39f52e76fb125b45c600785bcea0a711714e6
Antoine Vandevenne (anv) <anv@odoo.com>
2024-08-06 15:06:04 +0200

    chap 4 - many2many field

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -9,6 +9,7 @@
         'data/real_estate_property_type_data.xml',
         # Depends on `res_partner_data.xml`, `real_estate_property_type_data.xml`
         'data/real_estate_property_data.xml',
+        'data/real_estate_tag_data.xml',
 
         # Security
         'security/ir.model.access.csv',
@@ -19,6 +20,7 @@
         'views/real_estate_offer_views.xml',
         'views/real_estate_property_views.xml',
         'views/real_estate_property_type_views.xml',
+        'views/real_estate_tag_views.xml',
     ],
     'application': True,
 }
